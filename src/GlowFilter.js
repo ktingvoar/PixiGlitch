3aa69b4cf7b943d28a0c1e26e0eadd9f02aa89de
ktingvoar <matt.smith@sbs.com.au>
2014-10-06 20:11:25 +1100

    added a blur property

--- src/GlowFilter.js
+++ src/GlowFilter.js
@@ -6,6 +6,7 @@ PIXI_GLITCH.GlowFilter = function () {
     this.passes = [this];
 
     this.uniforms = {
+        blur_w: {type: '1i', value: 8},
         dimensions: {type: '4fv', value: [0, 0, 0, 0]}
     };
 
@@ -13,8 +14,8 @@ PIXI_GLITCH.GlowFilter = function () {
         'precision mediump float;',
         'uniform vec4 dimensions;',
         'uniform sampler2D uSampler;',
+        'uniform int blur_w;',
         'varying vec2 vTextureCoord;',
-        'const int blur_w = 10;',
         'const float strength = 1.3;',
         'void main (void)',
         '{',
@@ -22,7 +23,8 @@ PIXI_GLITCH.GlowFilter = function () {
         '   vec4 col = texture2D(uSampler, vTextureCoord);',
         '   vec4 gws = vec4(0.0, 0.0, 0.0, 1.0);',
         '   float weight = 1.0 / (float(blur_w) * 2.0 + 1.0) / (float(blur_w) * 2.0 + 1.0);',
-        '   for (int i = 0; i < blur_w * blur_w; i++) {',
+        '   for (int i = 0; i < 1024; i++) {',
+        '       if (i > blur_w * blur_w) {break;}',
         '       float miw = float(mod(float(i), float(blur_w)));',
         '       float idw = float(i / blur_w);',
         '       vec2 v1 = vec2(pos.x + miw, pos.y + idw);',
@@ -44,3 +46,12 @@ PIXI_GLITCH.GlowFilter.prototype = Object.create(PIXI.AbstractFilter.prototype);
 PIXI_GLITCH.GlowFilter.prototype.constructor = PIXI_GLITCH.GlowFilter;
 
 
+Object.defineProperty(PIXI_GLITCH.GlowFilter.prototype, 'blur', {
+    get: function() {
+        return this.uniforms.blur_w.value;
+    },
+    set: function(value) {
+        this.dirty = true;
+        this.uniforms.blur_w.value = Math.floor(value);
+    }
+});
